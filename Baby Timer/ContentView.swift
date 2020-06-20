1ce9a2f6f467c9fc5ea3e6ca26bea77d281b9447
Christopher Lang <chrislang015@gmail.com>
2020-06-20 11:37:01 +0930

    Initial Commit

---
+++ Baby Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Baby Timer
+//
+//  Created by Christopher Lang on 20/6/20.
+//  Copyright © 2020 Christopher Lang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
